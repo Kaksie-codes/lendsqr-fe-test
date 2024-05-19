0ba98e48182980585f21488a87a3243ea013ce52
Nsikak Akpan <builtbykaksy@gmail.com>
2024-05-19 16:17:12 +0100

    Initial Setup
    React installed, Assets imported from figma file and scss initialized.

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './app.scss'
+
+function App() {
+  
+
+  return (
+    <div className='hello-scss'>
+      Hello
+      <p className='specials'>
+        Continue
+      </p>
+    </div>
+  )
+}
+
+export default App
